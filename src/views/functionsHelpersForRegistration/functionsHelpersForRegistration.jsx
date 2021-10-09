4cc0b68525563193b145ec9ef555b5c1739318a3
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-10-09 16:12:46 +0300

    fix error message in input form

--- src/views/functionsHelpersForRegistration/functionsHelpersForRegistration.jsx
+++ src/views/functionsHelpersForRegistration/functionsHelpersForRegistration.jsx
@@ -8,25 +8,20 @@ const disabled = (isSubmitting, errors, touched) => {
 };
 
 const name = () =>
-  Yup.string()
-    .min(3, 'Invalid name. Name should have at least 3 symbol')
-    .required('Required');
+  Yup.string().min(3, 'should have at least 3 symbol').required('Required');
 
 const email = () =>
   Yup.string().email('Invalid email address').required('Required');
 
 const password = () =>
   Yup.string()
-    .min(7, 'Invalid password. Password should be greater then 7 symbols')
-    .max(15, 'Invalid password. Password should be less then 15 symbols')
+    .min(7, 'should be greater then 7 symbols')
+    .max(15, 'should be less then 15 symbols')
     .required('Required');
 
 const confirmPassword = () =>
   Yup.string()
-    .oneOf(
-      [Yup.ref('password'), null],
-      'Confirm password shoul be equal to password'
-    )
+    .oneOf([Yup.ref('password'), null], 'shoul be equal to password')
     .required('Required');
 
 const authHalpers = {
